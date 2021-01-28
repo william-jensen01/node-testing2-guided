654f3554776ab20b544cef30f268d5656399ba30
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-28 19:01:52 +0100

    wip

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -41,7 +41,7 @@ describe('Hobbits model', () => {
     it('updates the hobbit', async () => {
       // ?
       // using db, get a hobbit in the database
-      await db('hobbitsw')
+      await db('hobbits').insert(frodo)
       // await for the update of said hobbit
       // using db, retrieve the hobbit
       // check the change took
