04a7a3129278f0d315f86c4848095e84d5c04f15
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-28 19:16:30 +0100

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -2,4 +2,20 @@ const request = require('supertest')
 const db = require('../data/dbConfig')
 const server = require('./server')
 
+const frodo = { name: 'frodo' }
+const sam = { name: 'sam' }
 
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+beforeEach(async () => {
+  await db('hobbits').truncate()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
+describe('server', () => {
+  
+})
