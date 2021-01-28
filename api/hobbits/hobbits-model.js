9ab03879cbcf69d821ffe11eed22503706fa9644
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-28 18:48:16 +0100

    wip

--- api/hobbits/hobbits-model.js
+++ api/hobbits/hobbits-model.js
@@ -19,6 +19,7 @@ function getById(id) {
 async function insert(hobbit) {
   const [id] = await db('hobbits')
   .insert(hobbit)
+  return db('hobbits').where({ id })
 }
 
 async function update(id, changes) {
